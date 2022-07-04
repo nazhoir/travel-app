464c728fab92cd2192cd48d7d6da6c2da8bab47c
nazhoir <nazhoirulamin031201@gmail.com>
2022-07-05 04:05:22 +0700

    first supabase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
-// import 'package:travel_app/views/home.dart';
 import 'package:travel_app/views/landing.dart';
+import 'package:supabase/supabase.dart';
 
-void main() {
+Future<void> main() async {
+  final client = SupabaseClient('https://jvmnhdwqrpztylmhminx.supabase.co',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2bW5oZHdxcnB6dHlsbWhtaW54Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1Njk2ODA5MywiZXhwIjoxOTcyNTQ0MDkzfQ.peHIKCKXNz5v--IN_0W7QneYXgGnglYP9e45O1s5irc');
+
+  // Select from table `countries` ordering by `name`
+  final response = await client
+      .from('countries')
+      .select()
+      .order('name', ascending: true)
+      .execute();
   runApp(const MyApp());
 }
 
@@ -19,3 +28,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// 2fqN3dQ3r9p@396
